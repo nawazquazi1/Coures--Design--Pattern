e095ac3339499c819a1404b638711d586a81df33
nawaz quazi <nawazquazi356@gmail.com>
2023-05-30 20:54:02 +0530

    Design Pattern

---
+++ BuilderDesignPattern4/src/RobotBuilder.java
@@ -0,0 +1,16 @@
+// Defines the methods needed for creating parts 
+// for the robot
+
+public interface RobotBuilder {
+
+    void buildRobotHead();
+
+     void buildRobotTorso();
+
+     void buildRobotArms();
+
+     void buildRobotLegs();
+
+     Robot getRobot();
+
+}
